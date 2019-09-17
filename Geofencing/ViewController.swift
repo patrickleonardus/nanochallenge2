483f19c6c913d68e50b457ced64f1bb5d2faeea9
Patrick Leonardus <patrickleonardus@gmail.com>
2019-09-17 22:40:05 +0700

    Initial Commit

---
+++ Geofencing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Geofencing
+//
+//  Created by Patrick Leonardus on 17/09/19.
+//  Copyright © 2019 Patrick Leonardus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
